86ff821df458e2368ad0fbc101d55bc1e7533471
Iroha05-13 <100775168+Iroha05-13@users.noreply.github.com>
2022-04-10 15:44:38 +0000

    [Add]admin/items/new

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
 
+  namespace :admin do
+    resources :items, except: [:destroy]
+  end
   scope module: :public do
     root to: 'homes#top'
     get '/about' => 'homes#about', as: 'about'

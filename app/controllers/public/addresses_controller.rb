3be2c0b60ae74912db59ae9a901723c7ce6500a7
Iroha05-13 <100775168+Iroha05-13@users.noreply.github.com>
2022-04-17 13:13:13 +0000

    [Update]Cntroller public/addresses

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -1,7 +1,42 @@
 class Public::AddressesController < ApplicationController
   def index
+    @addresses = Address.all
+    @address = Address.new
   end
 
   def edit
+    @addresse = Address.find(params[:id])
+  end
+
+  def create
+    @address = Address.new(address_params)
+    if @address.save
+      redirect_to addresses_path
+    else
+      render :index
+    end
+  end
+
+  def update
+    @addresse = Address.find(params[:id])
+    if @address.update
+      redirect_to addresses_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @addresse = Address.find(params[:id])
+    if @address.destroy
+      redirect_to addresses_path
+    else
+      render :index
+    end
+  end
+
+  private
+  def address_params
+    params.require(:address).permit(:customer_id, :name, :postal_code, :address)
   end
 end

8a48f63655ea9c1199066288a9c3142c54930098
Iroha05-13 <100775168+Iroha05-13@users.noreply.github.com>
2022-04-24 13:48:09 +0000

    [Update]Controller public/orders

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,11 +1,20 @@
 class Public::OrdersController < ApplicationController
   def new
+    @order = Order.new
+  end
+
+  def create
   end
 
   def index
+    @orders = Order.all
   end
 
   def show
+    @order = Order.find(params[:id])
+  end
+
+  def confirm
   end
 
   def complete
--- app/models/order.rb
+++ app/models/order.rb
@@ -2,5 +2,7 @@ class Order < ApplicationRecord
 
     has_many :order_details, dependent: :destroy
     belongs_to :customer
+    
+    enum payment: { credit_card: 0, transfer: 1}
 
 end

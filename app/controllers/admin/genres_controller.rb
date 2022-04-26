16e251a5751ebc67b4fa4cde5c2c03b4d79a32ab
Iroha05-13 <100775168+Iroha05-13@users.noreply.github.com>
2022-04-26 04:59:24 +0000

    [Update]Controller admin/genres

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -3,20 +3,34 @@ class Admin::GenresController < ApplicationController
     @genres = Genre.all
     @genre = Genre.new
   end
-  
+
   def create
+    @genre = Genre.new(genre_params)
+    if @genre.save
+      flash[:notice] = "ジャンルが追加されました。"
+      redirect_to genres_path
+    else
+      render :index
+    end
   end
 
   def edit
     @genre = Genre.find(params[:id])
   end
-  
+
   def update
+    @genre = Genre.find(params[:id])
+    if @genre.update(genre_params)
+      flash[:notice] = "ジャンルが更新されました。"
+      redirect_to genres_path
+    else
+      render :edit
+    end
   end
-  
+
   private
-  
+
   def genre_params
-    params.require(:genre).permit(:)
+    params.require(:genre).permit(:name)
   end
 end

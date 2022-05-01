2b8772adf85a2018569cde5a94b2b660c2a4803f
Iroha05-13 <100775168+Iroha05-13@users.noreply.github.com>
2022-05-01 18:14:22 +0000

    [Update]Views admin/

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -1,18 +1,19 @@
 class Admin::OrderDetailsController < ApplicationController
 
   def update
-    @order = Order.find(params[:id])
     @order_detail = OrderDetail.find(params[:id])
+    @order = Order.find(params[:id])
     if @order_detail.update(detail_params)
       redirect_to admin_order_path(@order.id)
     else
+      @order = Order.find(params[:id])
       render :show
     end
   end
 
   private
   def detail_params
-    params.require(:order_detail).permit(:making_status)
+    params.require(:order_detail).permit(:order_id, :making_status)
   end
 
 end
--- app/controllers/public/registrations_controller.rb
+++ app/controllers/public/registrations_controller.rb
@@ -5,6 +5,10 @@ class Public::RegistrationsController < Devise::RegistrationsController
   before_action :authenticate_customer!, except: [:top, :about]
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  def after_sign_up_path_for(resource)
+    customers_path(current_customer.id)
+  end
+
   protected
 
   def configure_permitted_parameters
